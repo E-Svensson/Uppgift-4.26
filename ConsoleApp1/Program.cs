658eddea4fc535db8e1235ea486f82a6f88d379f
E-Svensson <edvin.sven123@gmail.com>
2022-10-21 10:22:19 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ____
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Skriv ut ett tal: "); string inputString = Console.ReadLine();
+
+            Console.WriteLine();
+
+            char[] inputArray = inputString.ToCharArray();
+
+            int[] outputArray = Array.ConvertAll(inputArray, c => (int)Char.GetNumericValue(c));
+
+            for (int i = 0; i < outputArray.Length; i++)
+            {
+                if (outputArray[i] < 9)
+                {
+                    outputArray[i]++;
+                }
+                else if (outputArray[i] == 9)
+                {
+                    outputArray[i] = 0;
+                }
+
+                Console.Write(outputArray[i]);
+            }
+        }
+    }
+}
